c237a583d9d22e9d7cb7889cbac50f3d2cb874b3
조호진 <hojin99@naver.com>
2022-03-16 21:31:10 +0900

    이미지 생성 방법 변경

---
+++ postgresql10/init_02_data_1.sql
@@ -0,0 +1,14 @@
+
+DO $$
+DECLARE
+    i INTEGER := 1;
+BEGIN
+    WHILE i < 100 loop
+    
+		insert into public.test_data
+		SELECT lfile_seq + i * 1000, dtime + i * INTERVAL '1 hour' dtime , gps_lon, gps_lat, tech_type, technology, technology_ca, qc_serving_pci, qc_serving_rsrp, qc_serving_sinr, qc_serving_rssi, bin_x, bin_y, operator_seq 
+		FROM public.init_data;
+
+        i := i + 1;
+    END LOOP;
+END $$;
